894c41d29b1f90cd23cdfd03714ebce057dab345
aniruddha789 <aniruddhaborkar45@gmail.com>
2023-11-05 00:40:50 +0530

    Resized image slider in ProductCard

--- frontend/footwear-retail-website/src/components/ProductCard/ProductCard.tsx
+++ frontend/footwear-retail-website/src/components/ProductCard/ProductCard.tsx
@@ -1,30 +1,28 @@
-import './ProductCard.css';
-import ImageSlider from '../ImageSlider/ImageSlider';
+import "./ProductCard.css";
+import ImageSlider from "../ImageSlider/ImageSlider";
 
 interface Props {
-
-    name: string;
-    text: string;
-    price: number;
-  
+  name: string;
+  text: string;
+  price: number;
 }
 
-function ProductCard( props : Props){
-
-    return (
-        
-        <div className="cardProduct" >
-          <ImageSlider/>
+function ProductCard(props: Props) {
+  return (
+    <div className="b">
+      <div className="cardProduct">
+        <div className="productImg">
+          <ImageSlider />
+        </div>
         {/* <img src="/src/assets/IMG20220127180432_01.jpg" width = "100%" className="card-img-top" alt="..."></img> */}
         <h4 className="card-title">{props.name}</h4>
-        <div className="card-body" >
-          <p className="card-text" >{props.text}</p>
+        <div className="card-body">
+          <p className="card-text">{props.text}</p>
           <p className="card-price"> ₹ {props.price}</p>
         </div>
-        
       </div>
-    )
-
+    </div>
+  );
 }
 
-export default ProductCard
+export default ProductCard;
