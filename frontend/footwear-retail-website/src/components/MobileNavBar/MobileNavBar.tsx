4b07d48e95b69e64310d4c74dbc998cf7fd446cb
aniruddha789 <aniruddhaborkar45@gmail.com>
2025-03-18 01:17:46 +0530

    Modify remove item icon for cart

--- frontend/footwear-retail-website/src/components/MobileNavBar/MobileNavBar.tsx
+++ frontend/footwear-retail-website/src/components/MobileNavBar/MobileNavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './MobileNavBar.css';
 import { MdOutlineHome, MdOutlineSearch, MdOutlinePerson, MdOutlineShoppingBag, MdOutlineCategory } from 'react-icons/md';
 import { useCart } from '../../context/CartContext';
