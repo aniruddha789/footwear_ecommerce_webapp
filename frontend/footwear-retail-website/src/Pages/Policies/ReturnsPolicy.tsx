330705d69092fb6a465d81667bd8f904d4266e2f
Aniruddha Borkar <41892662+aniruddha789@users.noreply.github.com>
2025-04-12 12:07:02 +0530

    Update ReturnsPolicy.tsx

--- frontend/footwear-retail-website/src/Pages/Policies/ReturnsPolicy.tsx
+++ frontend/footwear-retail-website/src/Pages/Policies/ReturnsPolicy.tsx
@@ -5,15 +5,13 @@ const ReturnsPolicy: React.FC = () => {
     <div>
       <h1>Returns Policy</h1>
       <p>Return Policy
-We offer refund / exchange within first 7 days from the date of your purchase. If 7 days have passed
-since your purchase, you will not be offered a return, exchange or refund of any kind. In order to become
+We offer return / exchange within first 7 days from the date of your purchase. If 7 days have passed
+since your purchase, you will not be offered a return or exchange of any kind. In order to become
 eligible for a return or an exchange, (i) the purchased item should be unused and in the same condition as
 you received it, (ii) the item must have original packaging, (iii) if the item that you purchased on a sale,
 then the item may not be eligible for a return / exchange. Further, only such items are replaced by us
-(based on an exchange request), if such items are found defective or damaged. (iv) Refund will be credited to your original payment method with in
- 5 to 7 business days. (v) Your order will be exchanged and replacement will delivered with in 5 to 7 business days.
-You agree that there may be a certain category of products / items that are exempted from returns or
-refunds. Such categories of the products would be identified to you at the item of purchase. For exchange
+(based on an exchange request), if such items are found defective or damaged. (iv) Your order will be exchanged and replacement will delivered with in 5 to 7 business days.
+You agree that there may be a certain category of products / items that are exempted from returns. Such categories of the products would be identified to you at the item of purchase. For exchange
 / return accepted request(s) (as applicable), once your returned product / item is received and inspected
 by us, we will send you an email to notify you about receipt of the returned / exchanged product. Further.
 If the same has been approved after the quality check at our end, your request (i.e. return / exchange) will
